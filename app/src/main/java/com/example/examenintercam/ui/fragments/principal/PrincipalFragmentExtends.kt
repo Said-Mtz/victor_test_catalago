9b5cdcbb79d0502a1a1062b59d23dda682f0ac3f
Said-Mtz <victorjr.mtz@hotmail.com>
2022-05-28 22:27:34 -0500

    [UPDATE]Modificando principal fragment para que infle details

--- app/src/main/java/com/example/examenintercam/ui/fragments/principal/PrincipalFragmentExtends.kt
+++ app/src/main/java/com/example/examenintercam/ui/fragments/principal/PrincipalFragmentExtends.kt
@@ -1,12 +1,10 @@
 package com.example.examenintercam.ui.fragments.principal
 
 import android.view.View
-import com.example.examenintercam.R
 import com.example.examenintercam.core.api.ApiBeerService
 import com.example.examenintercam.core.api.RetrofitService
 import com.example.examenintercam.core.model.BeerRequestModel
-import com.example.examenintercam.databinding.DialogDetailsBinding
-import com.example.examenintercam.ui.fragments.detail.DialogGenericFragment
+import com.example.examenintercam.ui.fragments.dialogfragmentdetails.DialogFragmentDetails
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -40,14 +38,20 @@ fun PrincipalFragment.initRecycler() {
         }
     })
     myAdapter.onItemClickListener = { itemList ->
-        DialogGenericFragment<DialogDetailsBinding>(R.layout.dialog_details){ mBinding, dialogFragment ->
+    /**Mostrando dialog fragment generico**/
+    /*DialogGenericFragment<DialogDetailsBinding>(R.layout.dialog_details){ mBinding, dialogFragment ->
             mBinding.apply {
                 imgBeer.setOnClickListener {
                     dialogFragment.dismissAllowingStateLoss()
                 }
 
             }
-        }.show(requireActivity().supportFragmentManager,"iodjiwedo")
+        }.show(requireActivity().supportFragmentManager,"iodjiwedo")*/
+
+        /**show my dialog fragment**/
+        val dialog = DialogFragmentDetails()
+        dialog.show(requireActivity().supportFragmentManager, "myDialogFragment")
+
         //Toast.makeText(requireActivity(), "Press $itemList", Toast.LENGTH_SHORT).show()
     }
 }
