6026f47af7bb4463740279bca279b089edac9ad1
Said-Mtz <victorjr.mtz@hotmail.com>
2022-05-28 22:25:02 -0500

    [UPDATE]Validando login

--- app/src/main/java/com/example/examenintercam/ui/fragments/login/LoginFragmentExtends.kt
+++ app/src/main/java/com/example/examenintercam/ui/fragments/login/LoginFragmentExtends.kt
@@ -7,19 +7,27 @@ import com.google.android.material.textfield.TextInputLayout
 
 fun LoginFragment.initElements() {
     mBinding.apply {
+
+        /*validFielFocusedUser()
+        validFielFocusedPassword()*/
+
         btnLogin.setOnClickListener {
-            /*val txtUser = edTxtUser.text.toString()
+            val txtUser = edTxtUser.text.toString()
             val txtPassword = edTxtPassword.text.toString()
-            validField(txtUser, inputLayoutEdTxtUser)
-            validField(txtPassword, inputLayoutEdTxtPassword)*/
 
-            //findNavController().navigate(R.id.action_loginFragment_to_principalFragment)
+            if(txtUser.isNotEmpty() && txtPassword.isNotEmpty()){
+                /**Alert Dialog user and password incorrect**/
+                /*val dialog = AlertDialogLoginObject
+                dialog.showDialog(requireActivity())*/
 
-            val dialog = AlertDialogLoginObject
-            dialog.showDialog(requireActivity())
+                /**Navegar a lista de cerveza**/
+                findNavController().navigate(R.id.action_loginFragment_to_principalFragment)
+            }else{
+                validField(txtUser, inputLayoutEdTxtUser)
+                validField(txtPassword, inputLayoutEdTxtPassword)
+            }
+
         }
-
-        /*validFieldUser()*/
     }
 }
 
@@ -29,18 +37,54 @@ fun LoginFragment.validField(string: String, edTxt: TextInputLayout) {
     }
 }
 
-private fun LoginFragment.validFieldUser() {
+private fun LoginFragment.validFielFocusedUser() {
     mBinding.edTxtUser.setOnFocusChangeListener { _, focused ->
-        if (!focused) {
-            mBinding.inputLayoutEdTxtUser.helperText = validTxtUser()
+        if (focused) {
+            mBinding.inputLayoutEdTxtUser.helperText = null
         }
     }
 }
 
-private fun LoginFragment.validTxtUser(): String {
+private fun LoginFragment.validFielFocusedPassword() {
+    mBinding.edTxtPassword.setOnFocusChangeListener { _, focused ->
+        if (focused) {
+            mBinding.inputLayoutEdTxtPassword.helperText = null
+        }
+    }
+}
+
+private fun LoginFragment.validFieldUser() {
+    mBinding.edTxtUser.setOnFocusChangeListener { _, focused ->
+        if (!focused) {
+            mBinding.inputLayoutEdTxtUser.helperText = validTxtUser()
+        }else{
+            mBinding.inputLayoutEdTxtUser.helperText = null
+        }
+    }
+}
+
+private fun LoginFragment.validTxtUser(): String? {
     val txtUser = mBinding.edTxtUser.text.toString()
     if (txtUser.isNullOrEmpty()) {
         return getString(R.string.txt_empty_field)
     }
-    return ""
+    return null
+}
+
+private fun LoginFragment.validFieldPassword() {
+    mBinding.edTxtPassword.setOnFocusChangeListener { _, focused ->
+        if (!focused) {
+            mBinding.inputLayoutEdTxtUser.helperText = validTxtPassword()
+        }else{
+            mBinding.inputLayoutEdTxtUser.helperText = null
+        }
+    }
+}
+
+private fun LoginFragment.validTxtPassword(): String? {
+    val txtUser = mBinding.edTxtUser.text.toString()
+    if (txtUser.isNullOrEmpty()) {
+        return getString(R.string.txt_empty_field)
+    }
+    return null
 }
