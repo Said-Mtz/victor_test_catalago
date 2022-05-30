f832536058286d9e277228a9880d63d2f73bdd7e
Victor <victorjr.mtz@hotmail.com>
2022-05-30 15:31:19 -0500

    [UPDATE] Login

--- app/src/main/java/com/example/examenintercam/ui/fragments/login/LoginFragmentExtends.kt
+++ app/src/main/java/com/example/examenintercam/ui/fragments/login/LoginFragmentExtends.kt
@@ -10,9 +10,6 @@ import java.util.regex.Pattern
 fun LoginFragment.initElements() {
     mBinding.apply {
 
-        /*validFielFocusedUser()
-        validFielFocusedPassword()*/
-
         btnLogin.setOnClickListener {
             val txtUser = edTxtUser.text.toString().trim().trimIndent()
             val txtPassword = edTxtPassword.text.toString().trim().trimIndent()
@@ -37,58 +34,6 @@ fun LoginFragment.validField(string: String, edTxt: TextInputLayout) {
         edTxt.helperText = getString(R.string.txt_empty_field)
     }
 }
-
-/*private fun LoginFragment.validFielFocusedUser() {
-    mBinding.edTxtUser.setOnFocusChangeListener { _, focused ->
-        if (focused) {
-            mBinding.inputLayoutEdTxtUser.helperText = null
-        }
-    }
-}
-
-private fun LoginFragment.validFielFocusedPassword() {
-    mBinding.edTxtPassword.setOnFocusChangeListener { _, focused ->
-        if (focused) {
-            mBinding.inputLayoutEdTxtPassword.helperText = null
-        }
-    }
-}
-
-private fun LoginFragment.validFieldUser() {
-    mBinding.edTxtUser.setOnFocusChangeListener { _, focused ->
-        if (!focused) {
-            mBinding.inputLayoutEdTxtUser.helperText = validTxtUser()
-        }else{
-            mBinding.inputLayoutEdTxtUser.helperText = null
-        }
-    }
-}
-
-private fun LoginFragment.validTxtUser(): String? {
-    val txtUser = mBinding.edTxtUser.text.toString()
-    if (txtUser.isNullOrEmpty()) {
-        return getString(R.string.txt_empty_field)
-    }
-    return null
-}
-
-private fun LoginFragment.validFieldPassword() {
-    mBinding.edTxtPassword.setOnFocusChangeListener { _, focused ->
-        if (!focused) {
-            mBinding.inputLayoutEdTxtUser.helperText = validTxtPassword()
-        }else{
-            mBinding.inputLayoutEdTxtUser.helperText = null
-        }
-    }
-}
-
-private fun LoginFragment.validTxtPassword(): String? {
-    val txtUser = mBinding.edTxtUser.text.toString()
-    if (txtUser.isNullOrEmpty()) {
-        return getString(R.string.txt_empty_field)
-    }
-    return null
-}*/
 
 private fun LoginFragment.validEmail(email: String): Boolean {
     val pattern: Pattern = Patterns.EMAIL_ADDRESS
