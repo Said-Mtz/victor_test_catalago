eaca5e32a27e409c503e11da94ba3f08083efa40
Victor <victorjr.mtz@hotmail.com>
2022-05-30 11:20:56 -0500

    [UPDATE] Details; pintando datos en la vista

--- app/src/main/java/com/example/examenintercam/ui/fragments/fragmentdetails/FragmentDetailsExtends.kt
+++ app/src/main/java/com/example/examenintercam/ui/fragments/fragmentdetails/FragmentDetailsExtends.kt
@@ -1,7 +1,29 @@
 package com.example.examenintercam.ui.fragments.fragmentdetails
 
-fun FragmentDetails.initElements(){
+fun FragmentDetails.initElements() {
     mBinding.apply {
 
+        beerItem = viewModel.getSelectedModel()
+        viewModel.getSelectedModel().apply {
+
+            txtLiters.text = "${volume.value} Lts"
+
+            txtAbvValue.text = "$abv %"
+            txtIbuValue.text = "${ibu.toInt()}"
+            txtOgValue.text = "${target_og.toInt()}"
+            txtFgValue.text = "$target_fg"
+
+            txtFirstBrewed.text = "First Brewed: $first_brewed"
+
+            txtFoodPairing.text = food_pairing.joinToString(separator = "\n-", prefix = "-")
+
+            txtHops.text = ingredients.hops.map {
+                "${it.name} ${it.amount.value} g."
+            }.joinToString(separator = "\n")
+
+            txtMalt.text = ingredients.malt.map {
+                "${it.name} ${it.amount.value} kg."
+            }.joinToString(separator = "\n")
+        }
     }
 }
